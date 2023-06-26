129dd6420c9980f31f7c9a5b4567bdd955789d1b
mlozanog96 <marylozanogzz@gmail.com>
2023-06-26 17:50:43 +0200

    data load test

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -1,11 +1,11 @@
 import streamlit as st
 #hints for debugging: https://awesome-streamlit.readthedocs.io/en/latest/vscode.html
-#import plotly.express as px
+import plotly.express as px
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import pickle
+import matplotlib.pyplot as plt
+import pickle
 import requests
-#import openai
+import openai
 from utils import get_data, get_indicator_reason, filter_projects
 
 
