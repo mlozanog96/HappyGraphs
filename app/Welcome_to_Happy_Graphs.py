5c2229e58f5a68fc60845dadba4fa69f3ec9c56d
mlozanog96 <marylozanogzz@gmail.com>
2023-06-26 18:54:02 +0200

    Update Welcome_to_Happy_Graphs.py

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -19,17 +19,3 @@ Below you see a line graph showcasing how life expectancy has been increasing fo
 
 Sidenote: Please don't get fooled by the decline of life expectancy since 2019. Research suggests that this is due to the Covid-19 pandemic, which has induced the first decline in global life expectancy since World War II. For further information, please read the [Nature Article](https://www.nature.com/articles/s41562-022-01450-3).
 """
-st.markdown(intro_text, unsafe_allow_html=True)
-
-df = pd.read_csv('world_bank_data.csv')
-available_indicators = df['indicator_name'].drop_duplicates().reset_index(drop=True)
-selected_indicator = st.selectbox("Select an indicator", available_indicators)
-
-df_indicator= df[df['indicator_name']==selected_indicator]
-available_countries = df_indicator['country'].drop_duplicates().reset_index(drop=True)
-selected_countries = st.multiselect("Select countries", available_countries, default=['World','Germany','Mexico']) #ACTION: make worldwide as a default
-
-min_year = int(df_indicator['date'].min())
-max_year = int(df_indicator['date'].max())
-selected_year_range = st.slider("Select a year range", min_value=min_year, max_value=max_year, value=(1990,max_year))
-selected_start_year, selected_end_year = selected_year_range
