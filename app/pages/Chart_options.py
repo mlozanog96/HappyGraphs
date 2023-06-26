e924b78a28171099dc8aae47ea97183e1f8ac3b4
mlozanog96 <marylozanogzz@gmail.com>
2023-06-26 23:20:26 +0200

    Filters copy

--- app/pages/Chart_options.py
+++ app/pages/Chart_options.py
@@ -1,23 +1,12 @@
 import streamlit as st
 #hints for debugging: https://awesome-streamlit.readthedocs.io/en/latest/vscode.html
-#import plotly.express as px
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import pickle
-import requests
-#import openai
-from utils import get_data, get_indicator_reason, filter_projects
-
-
-
-#with open('../pred_lifeexp.pkl', 'rb') as file:
-#    loaded_model = pickle.load(file)
 
 st.title('Happy Graphs')
 
 st.write("Group KMJ Do-Gooders proudly presents: Happy Graphs - Graphs which make us optimistic.")
 
-df= pd.read_csv('world_bank_data.csv')
+df= pd.read_csv('app/world_bank_data.csv')
 available_indicators = df['indicator_name'].drop_duplicates().reset_index(drop=True)
 selected_indicator = st.selectbox("Select an indicator", available_indicators)
 
--- app/pages/Explore_indicators.py
+++ app/pages/Explore_indicators.py
@@ -1,18 +1,12 @@
 import streamlit as st
+#hints for debugging: https://awesome-streamlit.readthedocs.io/en/latest/vscode.html
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import plotly.express as px
-#import pickle
-import requests
-#import openai
-#import plotly.graph_objects as go
-
 
 st.title('Happy Graphs')
 
 st.write("Group KMJ Do-Gooders proudly presents: Happy Graphs - Graphs which make us optimistic.")
 
-df= pd.read_csv('../app/world_bank_data.csv')
+df= pd.read_csv('app/world_bank_data.csv')
 available_indicators = df['indicator_name'].drop_duplicates().reset_index(drop=True)
 selected_indicator = st.selectbox("Select an indicator", available_indicators)
 
@@ -24,14 +18,3 @@ min_year = int(df_indicator['date'].min())
 max_year = int(df_indicator['date'].max())
 selected_year_range = st.slider("Select a year range", min_value=min_year, max_value=max_year, value=(1990,max_year))
 selected_start_year, selected_end_year = selected_year_range
-
-
-# Check if an indicator is selected
-if not selected_indicator:
-    st.text("Please choose an indicator to see its development.")
-else:
-    # Filter the data for selected countries and time period
-    filtered_data = df_indicator[['date','country','value']]
-    filtered_data = filtered_data[(filtered_data['date'] >= selected_start_year) & (filtered_data['date'] <= selected_end_year) & (df['country'].isin(selected_countries))]
-
-    st.dataframe(filtered_data)
