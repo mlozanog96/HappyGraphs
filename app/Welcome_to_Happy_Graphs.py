b71869e469803c0b0d6bc47cd798ec65bd32dc55
mlozanog96 <marylozanogzz@gmail.com>
2023-06-26 19:00:10 +0200

    Remove libraries

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -1,12 +1,6 @@
 import streamlit as st
 #hints for debugging: https://awesome-streamlit.readthedocs.io/en/latest/vscode.html
-import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
-import pickle
-import requests
-import openai
-from utils import get_data, get_indicator_reason, filter_projects
 
 st.title('Happy Graphs')
 
