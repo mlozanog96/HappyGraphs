2e984ae45f256f6f82e150aa719006d1cb1e147b
mlozanog96 <marylozanogzz@gmail.com>
2023-06-27 19:38:32 +0200

    Update Explore_indicators.py

--- app/pages/Explore_indicators.py
+++ app/pages/Explore_indicators.py
@@ -83,11 +83,16 @@ x_scale = alt.Scale(domain=(selected_start_year, selected_end_year), nice=False)
 y_scale = alt.Scale(domain=(filtered_data['value'].min(), filtered_data['value'].max()), nice=False)
 
 # Create an Altair line chart with tooltips
-chart = alt.Chart(data=filtered_data, mark='circle').mark_line().encode(
+chart = alt.Chart(data=filtered_data, mark="circle").mark_line().encode(
     x=alt.X('date:Q', scale=x_scale),
     y=alt.Y('value:Q', scale=y_scale),
     color='country',
     tooltip=['country', 'value']
+).properties(
+    width='container',
+    height=400
+).configure_axisX(
+    maxExtent=500
 )
 
 # Show the chart using Streamlit
