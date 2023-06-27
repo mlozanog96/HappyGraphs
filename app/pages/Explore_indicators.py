a42882a8d05d5e611a06fcbcfc66590504d3649a
mlozanog96 <marylozanogzz@gmail.com>
2023-06-27 19:19:40 +0200

    Update Explore_indicators.py

--- app/pages/Explore_indicators.py
+++ app/pages/Explore_indicators.py
@@ -22,11 +22,10 @@ selected_year_range = st.slider("Select a year range", min_value=min_year, max_v
 selected_start_year, selected_end_year = selected_year_range
 
 if not selected_countries:
-    selected_countries=['World']
+    selected_countries = ['World']
 
 # Filter the data for selected countries and time period
-filtered_data = df_indicator[['date','country','value']]
-filtered_data = filtered_data[(filtered_data['date'] >= selected_start_year) & (filtered_data['date'] <= selected_end_year) & (df['country'].isin(selected_countries))]
+filtered_data = df_indicator[(df_indicator['date'] >= selected_start_year) & (df_indicator['date'] <= selected_end_year) & (df_indicator['country'].isin(selected_countries))]
 
 # Group the data by country
 grouped_data = filtered_data.groupby('country')
@@ -34,14 +33,12 @@ grouped_data = filtered_data.groupby('country')
 # Create a line chart for each country
 fig, ax = plt.subplots()
 for country, data in grouped_data:
-    country_data = filtered_data[['Year', country]]
-    line_color = st.color_picker(f"Select color for {country}", key=country)
-    ax.plot(country_data['Year'], country_data[country], label=country, color=line_color)
+    ax.plot(data['date'], data['value'], label=country)
 
-     # Add a tooltip to show country and indicator value on hover
+    # Add a tooltip to show country and indicator value on hover
     tooltip = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
-                              bbox=dict(boxstyle="round", fc="white", edgecolor="gray"),
-                              arrowprops=dict(arrowstyle="->"))
+                          bbox=dict(boxstyle="round", fc="white", edgecolor="gray"),
+                          arrowprops=dict(arrowstyle="->"))
     tooltip.set_visible(False)
 
     def update_tooltip(event):
@@ -57,13 +54,11 @@ for country, data in grouped_data:
             fig.canvas.draw_idle()
 
 fig.canvas.mpl_connect("motion_notify_event", update_tooltip)
-    
+
 # Customize the chart
-ax.plot(data['date'], data['value'], label=country)
 ax.set_title(selected_indicator)
 ax.set_xlabel('Year')
 ax.set_ylabel(selected_indicator)
-ax.set_ylim(df[selected_indicator].min() - 10, df[selected_indicator].max() + 10)
 
 # Show the chart
 st.pyplot(fig)
