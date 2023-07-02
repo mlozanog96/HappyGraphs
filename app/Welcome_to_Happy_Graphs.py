fc917bf9c756dfa81997c2d9e41013d0a64164a8
mlozanog96 <marylozanogzz@gmail.com>
2023-07-02 17:09:34 +0200

    Update Welcome_to_Happy_Graphs.py

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -15,7 +15,7 @@ st.write("Group KMJ Do-Gooders proudly presents: Happy Graphs - Graphs which mak
 st.markdown("# Playground")
 
 # Load data
-df_countries_life_ex = pd.read_csv('app/prediction_model/data/default-data.csv')
+df_countries_life_ex = pd.read_csv('/prediction_model/data/default-data.csv')
 #df_countries_life_ex = pd.read_csv('https://raw.githubusercontent.com/mlozanog96/HappyGraphs/main/app/predicition%20model/data/default-data.csv?token=GHSAT0AAAAAACCPFVJG6RBT5L6NQ3D4J3KAZE652BQ')
 
 
