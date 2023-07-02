8ea3098fdf35ba8a5f96d93fd939a67b0cc0544c
mlozanog96 <marylozanogzz@gmail.com>
2023-07-02 23:54:57 +0200

    Update Welcome_to_Happy_Graphs.py

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -46,20 +46,6 @@ df= pd.read_csv('app/world_bank_data.csv')
 #available_indicators = df['indicator_name'].drop_duplicates().reset_index(drop=True)
 #selected_indicator = st.selectbox("Select an indicator", available_indicators)
 
-df_indicator= df[df['indicator_name']=='Life Expectancy']
-available_countries = df_indicator['country'].drop_duplicates().reset_index(drop=True)
-selected_countries = st.multiselect("Select countries", available_countries, default=['World','Germany','Mexico'])
-
-min_year = int(df_indicator['date'].min())
-max_year = int(df_indicator['date'].max())
-selected_year_range = st.slider("Select a year range", min_value=min_year, max_value=max_year, value=(1990,max_year))
-selected_start_year, selected_end_year = selected_year_range
-
-# Set the chart title and axis labels
-chart_title = "Life Expectancy"
-x_label = "Year"
-y_label = "Life Expectancy"
-
 filter_col1, filter_col2 = st.columns(2)
 available_indicators = df['indicator_name'].drop_duplicates().reset_index(drop=True)
 with filter_col1:
