68aa4599cbcb4bd8f77ed291ae138dcfb8a8bb33
mlozanog96 <marylozanogzz@gmail.com>
2023-07-06 00:39:49 +0200

    Update Welcome_to_Happy_Graphs.py

--- app/Welcome_to_Happy_Graphs.py
+++ app/Welcome_to_Happy_Graphs.py
@@ -98,9 +98,9 @@ st.write(matrix)
 ### Prediction with given features
 st.markdown("## Prediction")
 # User selection country
-countries = df_life_ex ['Country'].drop_duplicates().reset_index(drop=True)
-selected_country = st.selectbox("Select country for default values", countries, default='World')
-df_default= df_life_ex[df_life_ex['Country']==selected_country]
+selected_country = st.selectbox("Select countries", available_countries, default='World') 
+#selected_country = st.selectbox("Select country for default values", countries, default='World')
+df_default= df[df['Country']==selected_country]
 
 # Select default year
 available_years = df_default['Date'].drop_duplicates().reset_index(drop=True)
