43d89c55e9ac9a26d0f55a3685b6bd6de163546a
mlozanog96 <marylozanogzz@gmail.com>
2023-07-06 10:16:12 +0200

    Update Other Charts.py

--- app/pages/Other Charts.py
+++ app/pages/Other Charts.py
@@ -9,8 +9,7 @@ import json
 from github import Github
 
 st.title('Happy Grraphs')
-st.write("Group KMJ Do-Gooders proudly presents: Happy Graphs - Graphs which make us optimistic.")
-st.markdown('## Other charts')
+st.markdown('# Other charts')
 
 df= pd.read_csv('app/world_bank_data.csv')
 
