71f4dc42261273c84b448729661d83ed8f7c1ad5
mlozanog96 <marylozanogzz@gmail.com>
2023-07-07 08:45:25 +0200

    Update Other Happy Graphs.py

--- app/pages/Other Happy Graphs.py
+++ app/pages/Other Happy Graphs.py
@@ -83,7 +83,7 @@ df_indicator_radar= df_indicator_radar[df_indicator_radar['country'].isin(radar_
 available_years_radar=df_indicator_radar['date'].drop_duplicates().reset_index(drop=True)
 year=col2.selectbox("Select year",sorted(available_years_radar, reverse=True), index=1)
 df_radar= df_indicator_radar[df_indicator_radar['date']==year]
-df_radar=df_radar.drop('year',axis=1)
+df_radar=df_radar.drop('date',axis=1)
 
 st.write(df_radar)
 # Get unique indicators
