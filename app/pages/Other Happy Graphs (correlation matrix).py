600b55b1b7f8595a30581dc599becb24fdd998c5
mlozanog96 <marylozanogzz@gmail.com>
2023-07-16 22:20:00 +0200

    Update Other Happy Graphs (correlation matrix).py

--- app/pages/Other Happy Graphs (correlation matrix).py
+++ app/pages/Other Happy Graphs (correlation matrix).py
@@ -34,6 +34,8 @@ selected_start_year, selected_end_year = selected_year_range
 
 df_years=  df_countries[(df_countries['date'] >= selected_start_year) & (df_countries['date'] <= selected_end_year)]
 
+st.write(df_years)
+
 # Pivot the data to have indicators as columns and rows representing pairs of indicators
 pivoted_data = df_years.pivot(index='indicator_name', columns='indicator_name', values='value')
 
