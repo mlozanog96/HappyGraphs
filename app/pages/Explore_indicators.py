b3c2f87cdf9779bf22a214fd2fb2bd7d9a03e2ed
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-07-16 09:20:03 +0200

    testing

--- app/pages/Explore_indicators.py
+++ app/pages/Explore_indicators.py
@@ -118,7 +118,7 @@ st.dataframe(matrix)
 
 
 st.markdown('### Why has this indicator changed in the countries?')
-st.write('Disclaimer: The following explenation is generated using the model gpt 3.5 turbo by openai. For more information click here: https://platform.openai.com/docs/models/gpt-3-5')
+st.write('Disclaimer: The following explanation is generated using the model gpt 3.5 turbo by openai. For more information click here: https://platform.openai.com/docs/models/gpt-3-5')
 # Show the reason why it has that trend
 prompt_prep_trend = None
 for i, (country, trend_per_country) in enumerate(trends.items()):
